e4a0d0019ef95ffcd95d176661a26ccf0af6da43
SRU-INTERN <azt.sru.intern@gmail.com>
2025-09-08 18:50:37 +0200

    Update daads.py
    Signed-off-by: SRU-INTERN <azt.sru.intern@gmail.com>

--- daads.py
+++ daads.py
@@ -47,7 +47,7 @@ commodities = {
     }
 }
 
-@st.cache_data(ttl=3600)
+@st.cache
 def fetch_data(commodity_name, item_list, time_range):
     # Determine the start date based on the selected time range
     time_map = {
@@ -57,10 +57,11 @@ def fetch_data(commodity_name, item_list, time_range):
         '5Y': '-5Y'
     }
     start_date = time_map.get(time_range, '-1Y')  # Default to 1Y if invalid
-    
+    today_str = pd.Timestamp.today().strftime('%Y-%m-%d')
     # Fetch data for the selected time range
-    df = ds.get_data(tickers=",".join(item_list), start=start_date)
-    df.dropna(inplace=True)
+    df = ds.get_data(tickers=",".join(item_list), start=start_date, end=today_str)
+
+    
     
     # Fetch static data
     static_data = ds.get_data(tickers=",".join(item_list), fields=list(summary_fields.keys()), kind=0)
@@ -70,28 +71,45 @@ def fetch_data(commodity_name, item_list, time_range):
     return static_data, df
 
 def plot_commodity_data(name, static_data, df):
-    dates = pd.to_datetime(df.index)
-    X = mdates.date2num(dates)
     fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(20, 20))
     plt.subplots_adjust(hspace=0.5, wspace=0.3)
-    
+
     for index in range(df.shape[1]):
-        z = np.polyfit(X, np.array(df.iloc[:, index].values), 1)
-        p = np.poly1d(z)
         ax = axs[int(index/3), int(index%3)]
-        ax.plot(X, p(X), "r--")
-        ax.plot(X, df.iloc[:, index].values)
+
+        # ⬇️ Work per series to avoid NaN issues
+        s = df.iloc[:, index].dropna()
+        if s.empty:
+            ax.set_title("No recent data")
+            ax.axis('off')
+            continue
+
+        X = mdates.date2num(pd.to_datetime(s.index))
+        y = s.values
+
+        # Safe title lookup (works whether the column name is a tuple or a string)
+        name_key = s.name[0] if isinstance(s.name, (list, tuple)) else s.name
+        title = static_data.loc[name_key]['Name'] if (name_key in static_data.index and 'Name' in static_data.columns) else str(name_key)
+
+        # Trendline only if enough points
+        if len(s) >= 2:
+            z = np.polyfit(X, y, 1)
+            p = np.poly1d(z)
+            ax.plot(X, p(X), "r--")
+
+        ax.plot(X, y)
         loc = mdates.AutoDateLocator()
         years_fmt = mdates.DateFormatter('%Y-%m')
-        ax.set_title(static_data.loc[df.iloc[:, index].name[0]]['Name'])
+        ax.set_title(title)
         ax.xaxis.set_major_formatter(years_fmt)
         ax.xaxis.set_minor_locator(mdates.MonthLocator())
         ax.xaxis.set_major_locator(loc)
         for tick in ax.get_xticklabels():
             tick.set_rotation(45)
-    
+
     st.pyplot(fig)
     st.dataframe(static_data)
+
 
 # Streamlit app
 st.title('Commodity Overview Dashboard')
